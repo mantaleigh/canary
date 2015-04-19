547c304dd8020d39e8b7ed9453ac096093cba687
Jillianna Farietta <jillianna.farietta@gmail.com>
2015-04-19 03:33:04 +0000

    Changes made by Jillianna Farietta (jillianna.farietta@gmail.com).

--- care-naryHybrid/foodView/foodViewModel.js
+++ care-naryHybrid/foodView/foodViewModel.js
@@ -14,11 +14,21 @@ app.models.foodView.foodViewModel = (function() {
             var foodDate = $("#foodDateInput").val();
             var foodTime = $("#foodTimeInput").val();
             var foodNotes = $("#foodNotesInput").val();
-
+            
             // dateTime object
             var dateTime = new Date(foodDate + " " + foodTime);
-
+            
+            var el = new Everlive('TiQ179pLOVoq4iN1');
+            var data = el.data('Food');
+            data.create({ 'food_group' : foodGroup, 'food_name' : foodName, 'food_notes' : foodNotes, 'time' : dateTime},
+                function(data){
+                    //alert(JSON.stringify(data));
+                    console.log({ 'food_group' : foodGroup, 'food_name' : foodName, 'food_notes' : foodNotes, 'time' : dateTime});
+                },
+                function(error){
+                    alert(JSON.stringify(error));
+                });
         }
-    });
-    return viewModel;
+        });
+        return viewModel;
 })();
