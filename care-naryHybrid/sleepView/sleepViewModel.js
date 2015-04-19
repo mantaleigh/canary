4c13ace0eb03d339b4d5cd3b840ab82b9bd89b7f
mantaleigh <samanthalvoigt@gmail.com>
2015-04-18 23:26:43 -0400

    retrieve sleep form info

--- care-naryHybrid/sleepView/sleepViewModel.js
+++ care-naryHybrid/sleepView/sleepViewModel.js
@@ -6,8 +6,20 @@ app.models.sleepView.sleepViewModel = (function() {
             beginSleepDate: undefined,
             endSleepDate: undefined,
         },
-        submit: function() {},
-        cancel: function() {}
+        add: function() {
+
+            var awakeTime = $("#awakeTime").val();
+            var awakeDate = $("#awakeDate").val();
+            var asleepTime = $("#asleepTime").val();
+            var asleepDate = $("#asleepDate").val();
+
+            // dateTime object
+            var awakeDateTime = new Date(awakeDate + " " + awakeTime);
+            var asleepDateTime = new Date(asleepDate + " " + awakeTime);
+            console.log(awakeDateTime);
+            console.log(asleepDateTime);
+
+        }
     });
     return viewModel;
 })();
